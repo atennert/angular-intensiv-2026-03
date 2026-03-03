d9900f91b951b6234b9f4394f5b505890d47a7d3
Andreas Tennert <andreas@qupaya.com>
2026-03-03 09:50:24 +0100

    Show a list of Books

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,11 +9,23 @@ import { Book } from './book';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  book: Book = {
-    title: 'How to win friends',
-    author: 'Dale Carnegie',
-    abstract: 'In this book ...'
-  };
+  books: Book[] = [
+    {
+      title: 'How to win friends',
+      author: 'Dale Carnegie',
+      abstract: 'How to Win Friends and Influence ...'
+    },
+    {
+      title: 'The Willpower Instinct: How Self-Control Works ...',
+      author: 'Kelly McGonigal',
+      abstract: 'Based on Stanford University ...'
+    },
+    {
+      author: 'Simon Sinek',
+      title: 'Start with WHY',
+      abstract: "START WITH WHY shows that the leaders who've ..."
+    }
+  ];
 
   protected goToBookDetails(book: Book) {
     console.log('Navigate to book details, soon...');
